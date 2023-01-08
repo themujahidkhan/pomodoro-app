39164e3c0121fa580f85fa59af2b9e85af153b0f
themujahidkhan <mujucoder@gmail.com>
2023-01-08 20:20:29 +0530

    Implemented Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
 
 function Timer() {
-  const [timer, settimer] = useState("25:00");
-  function startTimer() {}
+  const [time, setTime] = useState([`${25} Minutes`, " : 00 Seconds"]);
+  let interval;
+
+  function startTimer() {
+    let i = 1500;
+    let minutes;
+    let secondsRemaining;
+
+    interval = setInterval(decrement, 1000);
+
+    function decrement() {
+      i = (i % 9000) - 1;
+      minutes = Math.floor(i / 60);
+      secondsRemaining = i - minutes * 60;
+
+      setTime([`${minutes} Minutes :`, ` ${secondsRemaining} Seconds`]);
+
+      if (minutes === 0 && secondsRemaining === 0) {
+        clearInterval(interval);
+        alert("Time is up");
+      }
+    }
+  }
+
+  function resetTimer() {
+    clearInterval(interval);
+    setTime([25, " : 00"]);
+  }
 
   return (
     <div>
@@ -11,10 +37,10 @@ function Timer() {
         <button>Short Break</button>
         <button>Long Break</button>
       </div>
-      <h1>{timer}</h1>
+      <h1>{time}</h1>
       <div>
-        <button>Start</button>
-        <button>Reset</button>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
