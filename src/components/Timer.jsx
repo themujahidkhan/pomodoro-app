1b66dc3e193b70897018fccde30d004b1981b215
themujahidkhan <mujucoder@gmail.com>
2023-01-15 23:05:12 +0530

    Timer finally working

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Timer.css";
 
 function Timer() {
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(5);
+  const [displayMessage, setDispalyMessage] = useState("");
 
   const minutesTimer = minutes < 10 ? `0${minutes}` : minutes;
   const secondsTimer = seconds < 10 ? `0${seconds}` : seconds;
+
+  const startTimer = useEffect(() => {
+    const interval = setInterval(() => {
+      clearInterval(interval);
+
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          setDispalyMessage("Session complete");
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [seconds]);
 
   function resetTimer() {
     setMinutes(0);
@@ -20,16 +38,14 @@ function Timer() {
         <button className="btn-shortBreak">Short Break</button>
         <button className="btn-longBreak">Long Break</button>
       </div>
-      <h1 className="displayTimer">
-        {minutesTimer}:{secondsTimer}
-      </h1>
+
+      {<h1>{displayMessage}</h1> && (
+        <h1 className="displayTimer">
+          {minutesTimer}:{secondsTimer}
+        </h1>
+      )}
       <div className="bottom-tab">
-        <button
-          className="btn-start"
-          onClick={() => {
-            alert("clicked");
-          }}
-        >
+        <button className="btn-start" onClick={startTimer}>
           Start
         </button>
         <button className="btn-stop" onClick={resetTimer}>
